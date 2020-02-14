9bc83004edae84a21f19bded3330b069a8738ecc
Joe Patton <umadjoe@gmail.com>
2020-02-13 22:53:48 -0500

    filter is working

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,18 +3,51 @@ var tableData = data;
 
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
+// Select the filter button
+var filter_button = d3.select("#filter-btn");
 
 
- // Step 5: Use d3 to update each cell's text with
- // weather report values (weekday, date, high, low)
+ // Use d3 to update each cell's text with
+ // UFO sighting values (datetime, city, state, country...etc)
  data.forEach(function(ufoSighting) {
-    console.log(ufoSighting);
     var row = tbody.append("tr");
     Object.entries(ufoSighting).forEach(function([key, value]) {
-      console.log(key, value);
       // Append a cell to the row for each value
       // in the weather report object
       var cell = row.append("td");
       cell.text(value);
     });
   });
+
+filter_button.on("click", function() {
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  
+  var filteredData = data.filter(sighting => sighting.datetime === inputValue);
+
+  console.log(filteredData);
+  // select table body and remove all rows
+  d3.select("tbody").selectAll("tr").remove();
+  
+  // Use d3 to update each cell's text with
+  // Filtered Rows
+  filteredData.forEach(function(ufoSighting) {
+  var row = tbody.append("tr");
+  Object.entries(ufoSighting).forEach(function([key, value]) {
+    console.log(key, value);
+    // Append a cell to the row for each value
+    // in the weather report object
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+
+
+});
+  
